cb6b7f1b3aa5b3924ef77f1bd983b45958b507d3
Andres Camilo Ocampo <andres_ocampo82211@elpoli.edu.co>
2023-01-30 19:55:00 -0500

    Version mejorada de fizebad project primera implementacion

---
+++ codigo.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const app = {
+    data() {
+        return {
+            indice: 1,
+            url: `imagenes/1.jpg`,
+            estado: false
+        }
+    },
+    mounted() {
+        this.cambiarImagen()
+    },
+    methods: {
+        cambiarImagen() {
+            setInterval(() => {
+                this.indice++
+                this.url = `imagenes/${this.indice}.jpg`
+                if (this.indice > 3) {
+                    this.indice = 1
+                    this.url = `imagenes/${this.indice}.jpg`
+                }
+            }, 3000)
+        },
+        crecer(id) {
+            if (this.estado) {
+                document.querySelector(id).style.justifyContent = "left"
+                document.querySelector(id).style.flexGrow = "1"
+                this.estado = false
+            } else {
+                document.querySelector(id).style.justifyContent = "center"
+                document.querySelector(id).style.flexGrow = "0"
+                this.estado = true
+            }
+        }
+    }
+}
+
+var mountedApp = Vue.createApp(app).mount('#app')
